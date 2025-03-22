c57b18f57460954851eaa247fb351a0e1e5fecc1
Kelno <kelnowm@gmail.com>
2025-03-22 04:36:48 +0100

    Validate data set JSON schema in VSCode

--- analog-brain-tool/src/interfaces/ICardSet.ts
+++ analog-brain-tool/src/interfaces/ICardSet.ts
@@ -3,11 +3,29 @@ import { ICard } from './ICard';
 
 export type SetId = string;
 
+/**
+   * A card set is a collection of cards that together form a decision tree.
+*/
 export interface ICardSet {
+  /**
+     * A unique string to identify the set. Won't be shown to the user.
+  */
   id: SetId;
+  /**
+     * Set title for the user to identify the set
+  */
   title: string;
+  /**
+     * Language code
+  */
   lang: LangId;
+  /**
+     * Whether this set should be the default one for the specified language.
+  */
   isDefaultForLanguage: boolean;
+  /**
+     * The list of cards in this set.
+  */
   cards: ICard[];
 }
 
