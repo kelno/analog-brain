12f436935add91362978299e941bf39e93c5e1d8
Kelno <kelnowm@gmail.com>
2025-04-04 23:59:28 +0200

    Start workig on Hamburger menu

---
+++ analog-brain-tool/src/components/Header/HamburgerMenuButton.tsx
@@ -0,0 +1,18 @@
+import Hamburger from 'hamburger-react';
+import React, { Dispatch, SetStateAction } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface HamburgerMenuButtonProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const HamburgerMenuButton: React.FC<HamburgerMenuButtonProps> = ({ isOpen, setIsOpen }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Hamburger label={t('hamburger.title')} toggled={isOpen} toggle={setIsOpen} />
+    </>
+  );
+};
--- analog-brain-tool/src/components/Header/Header.tsx
+++ analog-brain-tool/src/components/Header/Header.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LanguageSwitcher } from './LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
 import ColorSwitch from './ColorSwitch';
+import HamburgerMenuButton from './HamburgerMenuButton';
 
 const Header: React.FC = () => {
   const { t } = useTranslation();
 
+  const [hamburgerIsOpen, sethamburgerIsOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 w-full bg-slate-700 shadow-md z-50 h-header">
       <div className="mx-auto px-4 flex justify-between items-center py-4 h-full">
-        {/* Logo or App Name */}
-        <div className="text-2xl font-bold text-white">{t('title')}</div>
+        {/* Left side */}
+        <div className="flex items-center space-x-4">
+          {/* Hamburger Menu */}
+          <HamburgerMenuButton isOpen={hamburgerIsOpen} setIsOpen={sethamburgerIsOpen} />
 
-        {/* Menu Options */}
+          {/* Logo & App Name */}
+          <div className="text-2xl font-bold text-white">{t('title')}</div>
+        </div>
+
+        {/* Right side: Always shown options */}
         <div className="flex items-center space-x-6">
           <div className="flex items-center">
             <LanguageSwitcher />
@@ -24,6 +33,7 @@ const Header: React.FC = () => {
           </div>
         </div>
       </div>
+      {hamburgerIsOpen ? <div className="float-left">YAY BURGER MENU!!!</div> : null}
     </header>
   );
 };
