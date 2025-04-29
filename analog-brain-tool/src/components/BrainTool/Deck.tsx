ecad9a61d75e4caa0854c756bd824c4dbedff9e6
Kelno <kelnowm@gmail.com>
2025-04-29 20:05:11 +0200

    Remove the previous card on the braintool UI

--- analog-brain-tool/src/components/BrainTool/Deck.tsx
+++ analog-brain-tool/src/components/BrainTool/Deck.tsx
@@ -12,8 +12,6 @@ interface DeckProps {
 export const Deck: FC<DeckProps> = ({ deck }) => {
   const context = useBrainContext();
 
-  const previousCardId = context.getPreviousCard();
-
   const handleClickCard = (cardId: CardId, isPrevious: boolean) => {
     if (isPrevious) context.popCurrentCard();
     else context.selectCard(cardId, true);
@@ -29,15 +27,11 @@ export const Deck: FC<DeckProps> = ({ deck }) => {
   };
 
   const currentCardData = deck.cards.find((card) => card.id === context.currentCardId);
-  const previousCardData = deck.cards.find((card) => card.id === previousCardId);
 
   if (!currentCardData) return <></>;
 
   return (
-    <div
-      className="mx-8 py-20 mt-[var(--previous-card-offset-y)] flex flex-col-reverse"
-      style={{ height: 'calc(100vh - var(--spacing-header))' }}
-    >
+    <div className="m-8 flex flex-col-reverse" style={{ height: 'calc(100vh - var(--spacing-header))' }}>
       <div className="flex-none">
         <div className="flex justify-center py-4">
           <CardNavigation
@@ -48,14 +42,6 @@ export const Deck: FC<DeckProps> = ({ deck }) => {
         </div>
       </div>
       <div className="relative flex-col flex-auto">
-        {previousCardData && (
-          <div
-            id="previousCard"
-            className="absolute transform -translate-x-6 -translate-y-[var(--previous-card-offset-y)] opacity-50 w-full h-full"
-          >
-            <Card card={previousCardData} handleClickCard={handleClickCard} isPrevious />
-          </div>
-        )}
         <div id="currentCard" className="relative z-10 w-full h-full">
           <Card card={currentCardData} handleClickCard={handleClickCard} />
         </div>
