2934343d099ccd00fcc40c706fc54debd8de6458
Kelno <kelnowm@gmail.com>
2025-05-12 22:10:23 +0200

    Update eslint rules

--- analog-brain-tool/eslint.config.js
+++ analog-brain-tool/eslint.config.js
@@ -1,28 +1,50 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2024,
+      ecmaVersion: 'latest',
       globals: globals.browser,
     },
     plugins: {
+      react,
+      import: importPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        // You will also need to install and configure the TypeScript resolver
+        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+        typescript: true,
+        node: true,
+      },
     },
     rules: {
+      ...jsxA11y.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
+      ...reactRefresh.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': ['warn'],
       'prefer-const': ['warn'],
       'import/no-default-export': ['warn'],
+      'jsx-a11y/alt-text': 'error',
     },
   },
 );
--- analog-brain-tool/src/appContext/AppContextProvider.tsx
+++ analog-brain-tool/src/appContext/AppContextProvider.tsx
@@ -4,19 +4,21 @@ import { AppContext } from './AppContext';
 import { UrlManager } from '../utils/UrlManager/UrlManager';
 import { useTranslation } from 'react-i18next';
 import { UrlParams } from '../utils/UrlManager/UrlParams';
+//import { useAppStore } from './appStore';
 
 export const AppContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { i18n } = useTranslation();
+  //const { lang, setLanguage } = useAppStore();
 
   console.debug(`AppContextProvider: Render`);
 
   const urlLanguage = UrlManager.consumeParam(UrlParams.LANG);
-  const lang = urlLanguage ?? i18n.language;
+  const initialLang = urlLanguage ?? i18n.language;
 
-  if (i18n.language != lang) i18n.changeLanguage(lang);
+  if (i18n.language != initialLang) i18n.changeLanguage(initialLang);
 
   const [appState, setAppState] = useState<AppContextState>({
-    lang: lang,
+    lang: initialLang,
   });
 
   const appContext: AppContextData = new AppContextData(appState, setAppState, i18n);
