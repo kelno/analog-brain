ac219c003e0db84d83b8e77b27f01ab9d074052b
Kelno <kelnowm@gmail.com>
2025-05-13 20:57:12 +0200

    Update react router to v7

--- analog-brain-tool/src/main.tsx
+++ analog-brain-tool/src/main.tsx
@@ -2,16 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter
-      basename={import.meta.env.BASE_URL}
-      future={{
-        v7_relativeSplatPath: true,
-      }}
-    >
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
