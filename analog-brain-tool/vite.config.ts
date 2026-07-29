78923ba03bf4a01acb79b7d14845577cf5a72dfa
Kelno <kelnowm@gmail.com>
2026-07-29 16:08:09 +0200

    Formatting

--- analog-brain-tool/vite.config.ts
+++ analog-brain-tool/vite.config.ts
@@ -1,20 +1,17 @@
 /* eslint-disable import/no-default-export */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: "/analog-brain",
+  plugins: [react(), tailwindcss()],
+  base: '/analog-brain/',
   build: {
-    // make sure we have a fixed out dir for CI/CD
+    // Same as default but make sure we have a fixed output dir for CI/CD
     outDir: './dist',
   },
   define: {
-    BUILD_MS_TIME_SINCE_EPOCH: JSON.stringify(Date.now())
-  }
-})
+    BUILD_MS_TIME_SINCE_EPOCH: JSON.stringify(Date.now()),
+  },
+});
