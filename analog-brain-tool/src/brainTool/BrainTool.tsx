e30fe8d06c1f7458b83c6ec56dc9e31841d12fb8
Kelno <kelnowm@gmail.com>
2026-07-31 17:21:15 +0200

    Extract shared deck route component

--- analog-brain-tool/src/brainTool/BrainTool.tsx
+++ analog-brain-tool/src/brainTool/BrainTool.tsx
@@ -6,22 +6,22 @@ import { RouteFallback } from './routing/RouteFallback';
 import { useTranslation } from 'react-i18next';
 import { CARD_ROUTE_PATH, DECK_ROUTE_PATH } from './routing/routePaths';
 
+const DeckRoute = () => (
+  <DeckContextProvider>
+    <Deck />
+  </DeckContextProvider>
+);
+
 export const BrainTool = () => {
   const { t } = useTranslation();
 
   console.debug(`Rendering BrainTool`);
 
-  const deckRoute = (
-    <DeckContextProvider>
-      <Deck />
-    </DeckContextProvider>
-  );
-
   return (
     <Routes>
       <Route path="/" element={<DeckSelection />} />
-      <Route path={DECK_ROUTE_PATH} element={deckRoute} />
-      <Route path={CARD_ROUTE_PATH} element={deckRoute} />
+      <Route path={DECK_ROUTE_PATH} element={<DeckRoute />} />
+      <Route path={CARD_ROUTE_PATH} element={<DeckRoute />} />
       <Route
         path="*"
         element={
